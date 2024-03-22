2a8f0e1327500b9ad1177dfb0c8e18c26f2128aa
Ram Asodhi <ramcharan2510@gmail.com>
2024-03-22 11:01:29 -0400

    OAuth creation using firebase

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    profilePicture:{
+        type: String,
+        default: "https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg",
+    },
 },{timestamps: true}
 );
 
--- client/src/pages/sign-in/SignIn.jsx
+++ client/src/pages/sign-in/SignIn.jsx
@@ -3,6 +3,7 @@ import { Label, TextInput, Button, Alert, Spinner } from 'flowbite-react';
 import { useState } from 'react';
 import { useDispatch,useSelector } from 'react-redux'
 import { signInStart, signInSuccess, signInFailure } from '/src/redux/user/userSlice';
+import OAuth from '/src/components/OAuth.jsx';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -83,6 +84,7 @@ export default function SignIn() {
                 ): 'Sign In'
               }
           </Button>
+          <OAuth />
         </form>
         <div className='flex gap-2 text-sm mt-5'>
           <span>
