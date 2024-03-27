5245147e41e324c661ab19e30d7bad513235ba15
Ram Asodhi <ramcharan2510@gmail.com>
2024-03-27 05:42:13 -0400

    Profile Dashboard with complete UI of DashSidebar

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '/src/components/DashSidebar.jsx';
+import DashProfile from '/src/components/DashProfile.jsx';
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() =>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  }, [location.search])
+  return (
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* sidebar */}
+        <DashSidebar />
+      </div>
+      {/* Profile */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
+  );
 }
