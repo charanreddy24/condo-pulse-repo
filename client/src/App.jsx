7283a7ae1002b2965bffd8589c39215e08572e05
Ram Charan Reddy <52415965+charanreddy24@users.noreply.github.com>
2024-04-30 07:11:52 +0000

    protected signin route after user logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Dashboard from '/src/pages/dashboard/Dashboard.jsx';
-import Home from '/src/pages/home/Home.jsx';
-import About from '/src/pages/about/About.jsx';
-import SignIn from '/src/pages/sign-in/SignIn.jsx';
-import SignUp from '/src/pages/sign-up/SignUp.jsx';
-import NavbarHeader from '/src/components/NavbarHeader.jsx';
-import FooterComponent from '/src/components/FooterComponent.jsx';
-import PrivateRoute from '/src/components/PrivateRoute.jsx';
-import LandingPage from './pages/landingPage/LandingPage.jsx';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "/src/pages/dashboard/Dashboard.jsx";
+import Home from "/src/pages/home/Home.jsx";
+import About from "/src/pages/about/About.jsx";
+import SignIn from "/src/pages/sign-in/SignIn.jsx";
+import SignUp from "/src/pages/sign-up/SignUp.jsx";
+import NavbarHeader from "/src/components/NavbarHeader.jsx";
+import FooterComponent from "/src/components/FooterComponent.jsx";
+import PrivateRoute from "/src/components/PrivateRoute.jsx";
+import LandingPage from "./pages/landingPage/LandingPage.jsx";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <BrowserRouter>
       <NavbarHeader />
@@ -21,8 +23,14 @@ export default function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route
+          path="/sign-in"
+          element={currentUser ? <Navigate to="/" /> : <SignIn />}
+        />
+        <Route
+          path="/sign-up"
+          element={currentUser ? <Navigate to="/" /> : <SignUp />}
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <FooterComponent />
