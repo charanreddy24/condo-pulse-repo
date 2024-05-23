ea9648da2f7732d090cb2ea086ba5164ff6ad982
Ram Charan Reddy <52415965+charanreddy24@users.noreply.github.com>
2024-05-23 10:40:11 +0000

    Adding code sandbox Url to the socket codes and correction in MessageSkeleton

--- api/socket/socket.js
+++ api/socket/socket.js
@@ -1,7 +1,7 @@
-import { Server } from 'socket.io';
-import http from 'http';
-import express from 'express';
-import cors from 'cors';
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -9,8 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT'],
+    origin: ["http://localhost:5173", "https://yvx8pr-5173.csb.app/"],
+    methods: ["GET", "POST", "PUT"],
   },
 });
 
@@ -20,20 +20,20 @@ export const getReceiverSocketId = (receiverId) => {
 
 const userSocketMap = {}; //{userId: socketId}
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId != 'undefined') userSocketMap[userId] = socket.id;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
 
   //io.emit() is used to send events to all the connected clients
-  io.emit('getOnlineUsers', Object.keys(userSocketMap));
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   //socket.on is used to listen to the events, can be used on both client and server side
-  socket.on('disconnect', () => {
-    console.log('user disconnected', socket.id);
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
     delete userSocketMap[userId];
-    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
